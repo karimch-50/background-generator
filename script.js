b3658aa8652f3f4dc08e50093002f7d174eb45d3
karimch-50 <karimchaouki50@gmail.com>
2021-11-04 05:32:38 +0100

    hange the name on the css file and make an external js file

---
+++ script.js
@@ -0,0 +1,11 @@
+var css = document.querySelector("h3");
+var color1= document.querySelector(".color1");
+var color2= document.querySelector(".color2");
+var body = document.querySelector('body');
+
+function SetGredeint(){
+    body.style.background="linear-gradient(to right, "+color1.value+","+color2.value+")";
+    css.innerText = body.style.background;
+}
+color1.addEventListener("input",SetGredeint);
+color2.addEventListener("input",SetGredeint);
